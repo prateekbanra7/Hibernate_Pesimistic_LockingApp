eb1ccf443589e512d2898d5549ae3335954ddf5e
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-05-12 11:02:29 +0530

    Add files via upload

---
+++ LockingAppTest1.java
@@ -0,0 +1,46 @@
+package in.abc.main;
+
+import org.hibernate.LockMode;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.abc.model.InsurancePolicy;
+import in.abc.util.HibernateUtil;
+
+public class LockingAppTest1 {
+
+	public static void main(String[] args) {
+		
+		Session session = HibernateUtil.getSession();
+		Transaction transaction = null;
+		boolean flag = false;
+		
+		try {
+			
+			transaction = session.beginTransaction();
+			InsurancePolicy policy = session.get(InsurancePolicy.class, 1L, LockMode.UPGRADE_NOWAIT);
+			System.out.println(policy);
+			
+			Thread.sleep(30000);//30seconds(run lockingAppTest2.java now)
+			
+			//modify some data with policy objet
+			policy.setTenure(12);
+			flag = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Object updated...");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not updated...");
+			}
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+
+}
